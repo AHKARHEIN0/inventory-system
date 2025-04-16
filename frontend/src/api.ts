c3acd42cd0c95ae18103d295d047369e917238de
ahkarhein <arkxnova@gmail.com>
2025-04-16 12:38:20 +0800

    fix: correct API  URL

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://inventory-system-gg85.onrender.com/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
